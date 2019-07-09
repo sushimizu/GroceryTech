27897a6ed9dc29eda86f19d92bf30dc87daa9c5c
sushimizu <maki7shimizu@gmail.com>
2019-07-09 11:57:06 -0400

    Update login.py

--- login.py
+++ login.py
@@ -5,9 +5,10 @@ import requests
 import webbrowser
 
 
+webbrowser.open("buyerFunctionality6.html")
 r = requests.post("login1.html", data={'username','password'})
 #print (r.text)
-webbrowser.open("buyerFunctionality6.html")
+
 if data['username'] == 'buyer' :
 	webbrowser.open("buyerFunctionality6.html")
 else :
