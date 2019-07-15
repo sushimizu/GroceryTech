7420293607f600a229c50f1447ea125d9c124ed6
sushimizu <maki7shimizu@gmail.com>
2019-07-15 11:53:53 -0400

    Create buyerAccountInfo7.py

---
+++ templates/buyerAccountInfo7.py
@@ -0,0 +1,42 @@
+
+
+top = """ <!DOCTYPE HTML> 
+
+<html>
+	<head>
+		<title>Buyer Account Information </title>
+		<link rel="stylesheet" type="text/css" href="/static/style.css"/>
+	</head>
+<body>
+
+	
+	<h2>  Buyer Account Information </h2>
+	
+  
+  
+ 	<form action="buyerAccountInfo7.html">  """
+  
+  
+  bottom = 	"""  <input type="submit" value="update"> <br> <br>
+		
+	</form>
+	
+		
+		<form action="/buyerFunctionality">
+	  <input type="submit" value="Back" >
+		</form>
+    
+    <form action="/">
+	  <input type="submit" value="Delete Account" >
+		</form>
+	
+
+</body> """
+
+
+
+def buyerAccInfo():
+  text = top 
+  
+  
+  return text
