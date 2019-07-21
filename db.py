c72d3cdb97eb55b9f0d7f2241558b860cb32fc78
sushimizu <maki7shimizu@gmail.com>
2019-07-21 16:02:26 -0400

    Update db.py

--- db.py
+++ db.py
@@ -5,3 +5,9 @@ conn = pymysql.connect(host="localhost",
 							user="root",
 							passwd='password')
 cursor = conn.cursor()
+
+
+
+def insertBuyer():
+	query = "INSERT INTO Buyer (username, phone, address_id, default_payment, default_store_id) VALUES(%s,%d,%d,%s,%s)"
+	cursor.execute()
