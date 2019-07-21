3035ce34b84321f14bae74d8cf3297e0d4636ac4
Tobin Abraham <52385844+tobabraham@users.noreply.github.com>
2019-07-21 16:11:54 -0400

    Correct Login
    Made login work

--- db.py
+++ db.py
@@ -3,5 +3,33 @@ import pymysql
 conn = pymysql.connect(host="localhost",
 							db="GroceryTech",
 							user="root",
-							passwd='password')
+							passwd='CornyJoke12')
 cursor = conn.cursor()
+
+
+# returns 0 if credentials are invalid
+# returns 1 if user is a manager
+# returns 2 if user is NOT a manager
+def login(username, password):
+    query = "SELECT COUNT(*) FROM userr WHERE username = %s AND password = %s;"
+    response = cursor.execute(query, (username, password))
+    # clear cursor
+    cursor.fetchall()
+
+    if response == 0:
+        return 0
+    else:
+        query = "SELECT user_type FROM userr WHERE username = %s;"
+        response = cursor.execute(query, (username))
+
+        result = cursor.fetchone()
+
+        # sanity check
+        cursor.fetchall()
+
+        if result[0] == 'manager':  # if Is_manager
+            return 1
+        elif result[0] == 'deliverer':
+            return 2
+        else:
+            return 3
