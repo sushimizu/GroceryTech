384eabfc9c0ee8bb13b8c1fb16777d33287a04ac
sushimizu <maki7shimizu@gmail.com>
2019-07-22 15:55:48 -0400

    Update runThis.py

--- runThis.py
+++ runThis.py
@@ -127,8 +127,8 @@ def buyerAccountInfo():
 """
 fname = 'Fred'
 @app.route('/buyerAccountInfo', methods=['GET','POST'])
-def buyerAccountInfo(fname):
-	return render_template("buyerAccountInfo7.html", fname=fname)
+def buyerAccountInfo():
+	return render_template("buyerAccountInfo7.html")
 
 
 @app.route('/findItem', methods=['GET','POST'])
