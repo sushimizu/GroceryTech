5480cbb2e975da75fd6dc2ff96753e575ea78f48
sushimizu <maki7shimizu@gmail.com>
2019-07-23 18:51:03 -0400

    Update runThis.py

--- runThis.py
+++ runThis.py
@@ -319,13 +319,13 @@ def reciept():
 def orderHistory():
 	info = db.orderHist(currentUser)
 	isDel = []
-	"""
-	for i in info:
-		if int(i[5]) == 1:
+	
+	for i in info[5]:
+		if int(i) == 1:
 			isDel.append('Yes')
 		else:
 			isDel.append('No')
-	"""
+	
 	return render_template('orderHistory17.html', info=info, isDel=isDel)
 
 
