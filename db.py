b36c08a7cb4784e6350e8ead9f46af21adadba92
sushimizu <maki7shimizu@gmail.com>
2019-07-24 16:12:42 -0400

    Update db.py

--- db.py
+++ db.py
@@ -378,7 +378,7 @@ def popItem(itemName):
 
 
 def inventory(uname):
-	cursor.execute("SELECT Item.item_name, Item.description, Item.quantity, Item.listed_price, Item.wholesale_price Item.exp_date, SUM(Item.quantity) From Item Join soldAt on soldAt.item_id = Item.item_id WHERE soldAt.store_id IN (SELECT GroceryStore.store_id FROM GroceryStore Where GroceryStore.address_id IN (SELECT manages.store_address From manages WHERE manages.username=%s))",uname)
+	cursor.execute("SELECT Item.item_name, Item.description, Item.quantity, Item.listed_price, Item.wholesale_price, Item.exp_date, SUM(Item.quantity) From Item Join soldAt on soldAt.item_id = Item.item_id WHERE soldAt.store_id IN (SELECT GroceryStore.store_id FROM GroceryStore Where GroceryStore.address_id IN (SELECT manages.store_address From manages WHERE manages.username=%s))",uname)
 	info =  tuplesToList(cursor.fetchall())
 	return info
 
