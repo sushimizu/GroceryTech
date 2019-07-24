5c56432648de1f7586cbcba295d18aa8e93a4903
sushimizu <maki7shimizu@gmail.com>
2019-07-23 20:15:23 -0400

    Update runThis.py

--- runThis.py
+++ runThis.py
@@ -340,7 +340,7 @@ def orderHistory():
 			info[count,5] = 'No'
 		count = count +1
 	"""
-	print ( info.shape, info )
+	print (  info )
 	for i in info:
 		if int( i[5]) == 1:
 			temp = ('Yes'),
@@ -350,7 +350,7 @@ def orderHistory():
 			i = i + temp
 
 	#info.append(isDel)
-	print ( info.shape, info )
+	print (  info )
 	return render_template('orderHistory17.html', info=info, isDel=isDel)
 
 
